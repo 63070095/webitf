28e0433948b237a3f7756b7c0cb041e04dabe855
63070095 <73688535+63070095@users.noreply.github.com>
2020-10-30 15:57:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <head>
     <body>
         <h1>JOKE</h1>
-        <img src="doctor1.jpg">
+        <img src="doctor1.jpg" width="200">
         <p>คุณหมอคะ คือดิฉันตดบ่อยมากค่ะ ตดตลอดเวลา แต่ยังดีนะคะที่ตดของดิฉันไม่มีกลิ่น แล้วก็ไม่มีเสียงด้วย </p> 
         <p>บอกเลยนะคะว่าตั้งแต่เดินเข้าห้องมาเนี่ย ดิฉันตดไป 20 กว่าครั้งแล้วนะคะ </p>
         <p>แต่เพราะมันไม่มีกลิ่นไม่มีเสียง ดิฉันก็เลยไม่รู้สึกว่ามันเป็นปัญหาอะไร แต่ก็คิดว่ามาหาหมอสักหน่อยก็ดีอ่ะค่ะ"</p>
@@ -15,10 +15,10 @@
         <p>"คุณหมอ! คุณหมอเอายาอะไรให้ชั้นกินคะเนี่ย ตอนนี้ดิฉันก็ยังตดเงียบๆเหมือนเดิม แต่ตดมันเหม็นมากเลยค่ะ"</p>
         <p>"ดีมาก" คุณหมอตอบ</p>
         <p>"อาการคัดจมูกของคุณหายแล้วล่ะ...จากนี้ไปผมจะรักษาอาการหูตึงของคุณนะ"</p>
-        <img src="cat.jpg">
+        <img src="doctor2.jpg" width="200">
         <p></p>
         <br>
-        <p>>>></p><a href="https://www.youtube.com/watch?v=w0AOGeqOnFY&list=FLtBZgZgf627RIdOSqqYHFQA&index=8">Click here to open nice song</a>
+        <a href="https://www.youtube.com/watch?v=w0AOGeqOnFY&list=FLtBZgZgf627RIdOSqqYHFQA&index=8">Click here to open nice song</a>
         <h3>นิดา คงรุ่งเรือง (พิ้งค์) 63070095</h3>
     </body>
 </html>
